03099353d648c7c3e57e4fc52de00e4b08fb2b50
as-wanfang <sophie.fwan@gmail.com>
2019-01-10 10:09:45 +0800

    init

---
+++ franka_description/include/franka_description/franka_pick_place_server.h
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Udacity Inc.
+ *
+ * This file is part of Robotic Arm: Perception project for Udacity
+ * Robotics nano-degree program
+ *
+ * All Rights Reserved.
+ ******************************************************************************/
+
+// Author: Harsh Pandya
+
+#ifndef FRANKA_PICK_PLACE_SERVER_H
+#define FRANKA_PICK_PLACE_SERVER_H
+
+#include <string>
+#include <sstream>
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+#include <gazebo_msgs/GetModelState.h>
+
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/RobotTrajectory.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+#include <geometric_shapes/shape_operations.h>
+
+#include <eigen_conversions/eigen_msg.h>
+#include <franka_description/PickPlace.h>
+#include <tf/tf.h>
+
+class FRANKAPickPlace
+{
+public:
+  explicit FRANKAPickPlace(ros::NodeHandle nh);
+  ~FRANKAPickPlace();
+
+  bool Routine(franka_description::PickPlace::Request &req,
+                      franka_description::PickPlace::Response &res);
+
+private:
+  ros::NodeHandle nh_;
+
+  ros::ServiceClient client, grasp_client;
+
+  std::vector<geometry_msgs::Pose> grasp_list;
+  bool success;
+
+  const std::string ARM_PLANNING_GROUP = "panda_arm";
+  const std::string GRIPPER_PLANNING_GROUP = "panda_gripper";
+
+  const std::string DROPBOX_MESH_PATH =
+    "package://franka_description/models/dropbox/meshes/dropbox.dae";
+
+  std::vector<std::string> OBJECT_MESH_PATH_LIST;
+  const std::string OBJECT_1_MESH_PATH =
+    "package://franka_description/models/biscuits/meshes/biscuits.dae";
+  const std::string OBJECT_2_MESH_PATH =
+    "package://franka_description/models/soap/meshes/soap.dae";
+  const std::string OBJECT_3_MESH_PATH =
+    "package://franka_description/models/soap2/meshes/soap2.dae";
+  const std::string OBJECT_MESH_PATH =
+    "package://franka_description/models/";
+
+  moveit::planning_interface::MoveGroupInterface move_group;
+  moveit::planning_interface::MoveGroupInterface gripper_group;
+
+  moveit_visual_tools::MoveItVisualToolsPtr visual_tools_ptr;
+
+
+  const robot_state::JointModelGroup *joint_model_group;
+  const robot_state::JointModelGroup *gripper_joint_model_group;
+
+  // Define PlanningSceneInterface object to add and remove collision objects
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  moveit::planning_interface::MoveGroupInterface::Plan arm_plan;
+
+  ros::Publisher world_joint_pub;
+  /*
+   * Functions for gripper actuation
+   * close_gripper = 0; open gripper
+   *                 = 1; close gripper
+   */
+  bool OperateGripper(const bool &close_gripper);
+
+  bool SetupCollisionObject(const std::string &object_id,
+                            const std::string &mesh_path,
+                            const geometry_msgs::Pose &object_pose,
+                            moveit_msgs::CollisionObject &collision_object);
+
+  tf::Quaternion RPYToQuaternion(float R, float P, float Y);
+
+  bool IsPickPoseWithinLimits(geometry_msgs::Pose &pick_pose,
+                              geometry_msgs::Pose &act_obj_pose);
+};
+
+#endif  // FRANKA_PICK_PLACE_SERVER_H
---
+++ franka_description/src/franka_pick_place_server.cpp
@@ -0,0 +1,297 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Udacity Inc.
+ *
+ * This file is part of Robotic Arm: Perception project for Udacity
+ * Robotics nano-degree program
+ *
+ * All Rights Reserved.
+ ******************************************************************************/
+
+// Author: Harsh Pandya
+
+#include <franka_description/franka_pick_place_server.h>
+
+FRANKAPickPlace::FRANKAPickPlace(ros::NodeHandle nh)
+  : nh_(nh),
+    move_group(ARM_PLANNING_GROUP),
+    gripper_group(GRIPPER_PLANNING_GROUP)
+{
+  client = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+
+  // Pointer to JointModelGroup for improved performance.
+  joint_model_group =
+    move_group.getCurrentState()->getJointModelGroup(ARM_PLANNING_GROUP);
+  gripper_joint_model_group =
+    gripper_group.getCurrentState()->getJointModelGroup(GRIPPER_PLANNING_GROUP);
+
+  visual_tools_ptr.reset(new moveit_visual_tools::MoveItVisualTools("world"));
+  visual_tools_ptr->deleteAllMarkers();
+  visual_tools_ptr->loadRemoteControl();
+
+  // Create text marker for displaying current state
+  Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
+  text_pose.translation().z() = 1.6;
+  visual_tools_ptr->publishText(text_pose, "Welcome to Advance Pick and Place project",
+                           rviz_visual_tools::WHITE, rviz_visual_tools::XXXLARGE);
+
+  // Publish messages to rviz
+  visual_tools_ptr->trigger();
+
+  /*
+   * Collision Objects:
+   * Create an object list and populate it with dropbox objects
+   * Then insert objects in scene for collision avoidance and interaction
+   */
+  std::vector<moveit_msgs::CollisionObject> collision_object_list;
+  std::vector<std::string> object_ids;
+  moveit_msgs::CollisionObject dropbox_collision_object;
+
+  // Define pose for the objects (specified relative to base_footprint)
+  geometry_msgs::Pose mesh_pose;
+
+  mesh_pose.position.x = -0.2;
+  mesh_pose.position.y = -0.70;
+  mesh_pose.position.z = 0;
+  mesh_pose.orientation.w = 0.707;
+  mesh_pose.orientation.x = 0;
+  mesh_pose.orientation.y = 0;
+  mesh_pose.orientation.z = 0.707;
+
+  SetupCollisionObject("dropbox", DROPBOX_MESH_PATH, mesh_pose,
+                       dropbox_collision_object);
+
+  collision_object_list.push_back(dropbox_collision_object);
+
+  // Add the object list to the world scene
+  planning_scene_interface.addCollisionObjects(collision_object_list);
+  ROS_INFO("Added object list to the world");
+  ros::Duration(1.0).sleep();
+
+  //Ready to pick
+  move_group.setNamedTarget("place_pose");
+
+  success = move_group.move() == moveit::planning_interface::MoveItErrorCode::SUCCESS;
+
+  OperateGripper(false);
+  ros::Duration(5.0).sleep();
+
+}
+
+bool FRANKAPickPlace::Routine(franka_description::PickPlace::Request &req,
+                    franka_description::PickPlace::Response &res)
+{
+
+  // test
+  visual_tools_ptr->deleteAllMarkers();
+
+  // Create text marker for displaying current state
+  Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
+  text_pose.translation().z() = 1.6;
+  visual_tools_ptr->publishText(text_pose, "New request received",
+                           rviz_visual_tools::WHITE, rviz_visual_tools::XXXLARGE);
+  visual_tools_ptr->trigger();
+
+  //Is pick_pose close to actual object pose
+  gazebo_msgs::GetModelState srv;
+  geometry_msgs::Pose act_obj_pose, place_pose, grasp_pose;
+
+  grasp_pose = req.pick_pose;
+
+  srv.request.model_name = req.object_name.data;
+  srv.request.relative_entity_name = "world";
+
+  //if(!success)
+  if(1)
+  {
+    // Pick Pose is within limits, next spawn the collision object
+    geometry_msgs::Pose target_mesh_pose;
+    std::vector<moveit_msgs::CollisionObject> target_object_list;
+    std::vector<std::string> object_ids;
+    moveit_msgs::CollisionObject target_collision_object;
+
+    std::string TARGET_MESH_PATH = OBJECT_MESH_PATH + req.object_name.data + "/meshes/" + req.object_name.data + ".dae";
+
+    SetupCollisionObject(req.object_name.data, TARGET_MESH_PATH, act_obj_pose, target_collision_object);
+
+    target_object_list.push_back(target_collision_object);
+    // Add the object list to the world scene
+    planning_scene_interface.addCollisionObjects(target_object_list);
+    ROS_INFO("Added Target to the world");
+    ros::Duration(1.0).sleep();
+
+    // Plan arm motion
+    // set starting pose
+    move_group.setStartStateToCurrentState();
+
+    // set target pose
+    if(1)
+    {
+      move_group.setPoseTarget(grasp_pose);
+
+      success = move_group.plan(arm_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS;
+      ROS_INFO("Visualizing plan to target: %s",
+               success ? "SUCCEEDED" : "FAILED");
+
+      // We can also visualize the plan as a line with markers in Rviz.
+      ROS_INFO("Visualizing plan 1 as trajectory line");
+      visual_tools_ptr->publishAxisLabeled(grasp_pose, "reach_pose");
+      visual_tools_ptr->publishText(text_pose, "Reach Pose", rviz_visual_tools::WHITE, rviz_visual_tools::XLARGE);
+      visual_tools_ptr->publishTrajectoryLine(arm_plan.trajectory_, joint_model_group);
+      visual_tools_ptr->trigger();
+
+      move_group.execute(arm_plan);
+
+      //Reach movement
+      move_group.setStartStateToCurrentState();
+      grasp_pose.position.z = grasp_pose.position.z - 0.08;
+      move_group.setPoseTarget(grasp_pose);
+      success = move_group.plan(arm_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS;
+      ROS_INFO("Visualizing plan to target: %s",
+               success ? "SUCCEEDED" : "FAILED");
+
+      // We can also visualize the plan as a line with markers in Rviz.
+      visual_tools_ptr->deleteAllMarkers();
+      visual_tools_ptr->publishAxisLabeled(grasp_pose, "pick_pose");
+      visual_tools_ptr->publishText(text_pose, "Pick Pose", rviz_visual_tools::WHITE, rviz_visual_tools::XLARGE);
+      visual_tools_ptr->publishTrajectoryLine(arm_plan.trajectory_, joint_model_group);
+      visual_tools_ptr->trigger();
+
+      move_group.execute(arm_plan);
+
+      //Remove object from the scene
+      object_ids.push_back(req.object_name.data);
+      planning_scene_interface.removeCollisionObjects(object_ids);
+
+      //Close Gripper
+      OperateGripper(true);
+      ros::Duration(3.0).sleep();
+
+      //Reach movement
+      move_group.setStartStateToCurrentState();
+      grasp_pose.position.z = grasp_pose.position.z+0.14;
+      move_group.setPoseTarget(grasp_pose);
+      success = move_group.plan(arm_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS;
+      ROS_INFO("Visualizing plan to target: %s",
+               success ? "SUCCEEDED" : "FAILED");
+      // visualize the plan in Rviz.
+      visual_tools_ptr->deleteAllMarkers();
+      visual_tools_ptr->publishAxisLabeled(grasp_pose, "reach_pose");
+      visual_tools_ptr->publishText(text_pose, "Reach Pose", rviz_visual_tools::WHITE, rviz_visual_tools::XLARGE);
+      visual_tools_ptr->publishTrajectoryLine(arm_plan.trajectory_, joint_model_group);
+      visual_tools_ptr->trigger();
+
+      move_group.execute(arm_plan);
+
+      //drop the ball
+      move_group.setStartStateToCurrentState();
+
+      move_group.setNamedTarget("place_pose");
+      success = move_group.plan(arm_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS;
+      ROS_INFO("Visualizing plan to target: %s",
+               success ? "SUCCEEDED" : "FAILED");
+
+      visual_tools_ptr->publishText(text_pose, "Drop Pose", rviz_visual_tools::WHITE, rviz_visual_tools::XLARGE);
+      visual_tools_ptr->publishTrajectoryLine(arm_plan.trajectory_, joint_model_group);
+      visual_tools_ptr->trigger();
+      move_group.execute(arm_plan);
+
+
+      //Open Gripper
+      OperateGripper(false);
+      ros::Duration(5.0).sleep();
+
+      //Raise arms
+      // move_group.setNamedTarget("PLACE_POSE");
+      // success = move_group.move() == moveit::planning_interface::MoveItErrorCode::SUCCESS;
+    }
+
+    res.success = true;
+  }
+}
+
+
+bool FRANKAPickPlace::SetupCollisionObject(const std::string &object_id,
+    const std::string &mesh_path,
+    const geometry_msgs::Pose &object_pose,
+    moveit_msgs::CollisionObject &collision_object)
+{
+  collision_object.header.frame_id = move_group.getPlanningFrame();
+  collision_object.id = object_id;
+
+  shapes::Mesh* m = shapes::createMeshFromResource(mesh_path);
+
+  ROS_DEBUG_STREAM(object_id << " mesh loaded");
+
+  shape_msgs::Mesh object_mesh;
+  shapes::ShapeMsg object_mesh_msg;
+  shapes::constructMsgFromShape(m, object_mesh_msg);
+  object_mesh = boost::get<shape_msgs::Mesh>(object_mesh_msg);
+  collision_object.meshes.resize(1);
+  collision_object.mesh_poses.resize(1);
+  collision_object.meshes[0] = object_mesh;
+
+  collision_object.mesh_poses[0].position = object_pose.position;
+  collision_object.mesh_poses[0].orientation = object_pose.orientation;
+
+  collision_object.meshes.push_back(object_mesh);
+  collision_object.mesh_poses.push_back(collision_object.mesh_poses[0]);
+  collision_object.operation = collision_object.ADD;
+}
+
+bool FRANKAPickPlace::OperateGripper(const bool &close_gripper)
+{
+  // RobotState contains the current position/velocity/acceleration data
+  moveit::core::RobotStatePtr gripper_current_state =
+    gripper_group.getCurrentState();
+
+  // Next get the current set of joint values for the group.
+  std::vector<double> gripper_joint_positions;
+  gripper_current_state->copyJointGroupPositions(gripper_joint_model_group,
+      gripper_joint_positions);
+
+  ROS_DEBUG("No. of joints in eef_group: %zd", gripper_joint_positions.size());
+
+  // Set finger joint values
+  if (close_gripper)
+  {
+    gripper_joint_positions[0] = 0.0;
+    gripper_joint_positions[1] = 0.0;
+  }
+  else
+  {
+    gripper_joint_positions[0] = 0.04;
+    gripper_joint_positions[1] = 0.04;
+  }
+
+  gripper_group.setJointValueTarget(gripper_joint_positions);
+  ros::Duration(1.5).sleep();
+
+  bool success = gripper_group.move() == moveit::planning_interface::MoveItErrorCode::SUCCESS;
+  return success;
+}
+
+tf::Quaternion FRANKAPickPlace::RPYToQuaternion(float R, float P, float Y)
+{
+  tf::Matrix3x3 mat;
+  mat.setEulerYPR(Y,P,R);
+
+  tf::Quaternion quat;
+  mat.getRotation(quat);
+
+  return quat;
+}
+
+FRANKAPickPlace::~FRANKAPickPlace(){}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "franka_pick_place_server");
+  ros::NodeHandle nh;
+  ros::AsyncSpinner spinner(8);
+  spinner.start();
+  FRANKAPickPlace franka_pick_place(nh);
+  ros::ServiceServer service = nh.advertiseService("pick_place_routine", &FRANKAPickPlace::Routine, &franka_pick_place);
+  ros::waitForShutdown();
+  return 0;
+}
